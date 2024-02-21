169e9aeb18e41a3191781d2426c38c6368de5d80
john.barsigo@student.moringaschool.com <john.barsigo@student.moringaschool.com>
2024-02-21 13:35:54 +0300

    Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,37 @@
-// create your App component here
+import { useEffect, useState } from "react";
+
+function App () {
+
+    const [ img,  setImg ] = useState("");
+    const [ pageLoading, setPageLoading ] = useState(true);
+
+    const url = "https://dog.ceo/api/breeds/image/random";
+
+    useEffect (() => {
+
+        function fetchImage () {
+            fetch (url)
+                .then ( (res) => res.json() )
+                .then ( (data) => { 
+                    setImg(data.message);
+                    (setPageLoading(false))
+                } )
+
+            };
+        fetchImage();
+        }, []);
+
+    return (
+        <>
+            {
+                pageLoading ? 
+                (<p>Loading...</p>)
+                : (
+                    <img src={img} alt="A Random Dog" ></img>
+                )
+            }
+        </>
+    );
+}
+
+export default App;
